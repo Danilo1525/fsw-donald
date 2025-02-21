0020e7cba258688b1db181c5b77db118c69372d7
Danilo Martinez <danilomartinezferreira@gmail.com>
2025-02-21 17:00:14 -0400

    chore: add eslint sort lugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,11 +2,13 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 const ProductsPage = () => {
-    return <div className="p-5 border border-red-500 rounded-xl">
-        <h1 className="text-red-500">products page</h1>
-        <Button>FSW 7.0</Button>
-        <Input placeholder="Porgramação é muito demorado"/>
+  return (
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">products page</h1>
+      <Button>FSW 7.0</Button>
+      <Input placeholder="Porgramação é muito demorado" />
     </div>
-}
- 
+  );
+};
+
 export default ProductsPage;
