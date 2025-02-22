757147980bc26e02f69c8b60835035b0dbb20114
Danilo Martinez <danilomartinezferreira@gmail.com>
2025-02-22 14:09:23 -0400

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,7 +1,9 @@
+import { formatCurrency } from "@/helpers/format-currency";
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { format } from "path";
 
 interface ProductsProps {
   products: Product[];
@@ -24,10 +26,7 @@ const Products = ({ products }: ProductsProps) => {
               {product.description}
             </p>
             <p className="pt-3 text-sm font-semibold">
-              {new Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }).format(product.price)}
+              {formatCurrency(product.price)}
             </p>
           </div>
 
