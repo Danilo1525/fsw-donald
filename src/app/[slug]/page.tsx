10383646eb4ebdb224e3c73dd9ad9eafe03c893d
Danilo Martinez <danilomartinezferreira@gmail.com>
2025-02-21 23:33:55 -0400

    feat: add initial restaurant page  and menu page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,14 +1,15 @@
-import { db } from "@/lib/prisma";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-import { RestaurantPageProps } from "./[slug]/page";
 
-const HomePage = () => {
-  return <h1>hello world</h1>;
-};
+import { db } from "@/lib/prisma";
 
-export default HomePage;
-export const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
   const restaurant = await db.restaurant.findUnique({ where: { slug } });
   if (!restaurant) {
@@ -16,19 +17,19 @@ export const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   }
   return (
     <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
-      {/*LOGO E TITULO*/}
+      {/* LOGO E TITULO */}
       <div className="flex flex-col items-center gap-2">
         <Image
-          src={restaurant?.avatarImageUrl}
+          src={restaurant.avatarImageUrl}
           alt={restaurant.name}
           width={82}
           height={82}
         />
         <h2 className="font-semibold">{restaurant.name}</h2>
       </div>
-      {/*BEM VINDO*/}
+      {/* BEM VINDO */}
       <div className="space-y-2 pt-24 text-center">
-        <h3 className="semibold text-2xl">Seja bem-vindo!</h3>
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
         <p className="opacity-55">
           Escolha como prefere aproveitar sua refeição. Estamos aqui para
           oferecer praticidade e sabor em cada detalhe!
@@ -36,11 +37,15 @@ export const RestaurantPage = async ({ params }: RestaurantPageProps) => {
       </div>
       <div className="grid grid-cols-2 gap-4 pt-14">
         <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
           buttonText="Para comer aqui"
           imageAlt="Comer aqui"
-          imageUrl="/dine-in.png"
+          imageUrl="/dine_in.png"
         />
         <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
           buttonText="Para levar"
           imageAlt="Para levar"
           imageUrl="/takeaway.png"
@@ -49,3 +54,5 @@ export const RestaurantPage = async ({ params }: RestaurantPageProps) => {
     </div>
   );
 };
+
+export default RestaurantPage;
